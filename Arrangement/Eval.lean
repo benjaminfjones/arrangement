5d5c6f0333651e5d5e9ee682c6eb102196e630f3
Benjamin F Jones <benjaminfjones@gmail.com>
2026-01-03 11:36:07 -0800

    Add more primitives

--- Arrangement/Eval.lean
+++ Arrangement/Eval.lean
@@ -9,18 +9,40 @@ open LispVal
 -- Evaluation: Code data type to Data data type
 ------------------------------------------------------------------------
 
-def unwrapNumber : LispVal → ThrowsError Nat
+def unwrapNum : LispVal → ThrowsError Nat
   | .number n => pure n
   | notNum => throwError $ LispError.typeMismatch "number" notNum
+
+def unwrapStr : LispVal → ThrowsError String
+  | .string s => pure s
+  | notNum => throwError $ LispError.typeMismatch "string" notNum
+
+def unwrapBool : LispVal → ThrowsError Bool
+  | .bool b => pure b
+  | notNum => throwError $ LispError.typeMismatch "bool" notNum
 
 def numericBinop (op : Nat → Nat → Nat) (args : List LispVal) : ThrowsError LispVal :=
   match args with
   | [] => throwError $ LispError.numArgs 2 []
   | singleVal@([_]) => throwError $ LispError.numArgs 2 singleVal
   | hd :: tl => do
-    let uhd ← unwrapNumber hd
-    let utl ← List.mapM unwrapNumber tl
+    let uhd ← unwrapNum hd
+    let utl ← List.mapM unwrapNum tl
     pure $ LispVal.number (List.foldl op uhd utl)
+
+def boolBinop (unpacker : LispVal → ThrowsError a) (op : a → a → Bool) (args : List LispVal) : ThrowsError LispVal :=
+  if h : args.length != 2 then
+    throwError $ LispError.numArgs 2 args
+  else do
+    have h0 : args.length > 0 := by grind
+    have h1 : args.length > 1 := by grind
+    let left ← unpacker (args[0]'h0)
+    let right ← unpacker (args[1]'h1)
+    pure ∘ LispVal.bool $ op left right
+
+def numBoolBinop : (Nat → Nat → Bool) → List LispVal → ThrowsError LispVal := boolBinop unwrapNum
+def boolBoolBinop : (Bool → Bool → Bool) → List LispVal → ThrowsError LispVal := boolBinop unwrapBool
+def strBoolBinop : (String → String → Bool) → List LispVal → ThrowsError LispVal := boolBinop unwrapStr
 
 def primitives : List (String × (List LispVal -> ThrowsError LispVal)) :=
   [
@@ -30,6 +52,19 @@ def primitives : List (String × (List LispVal -> ThrowsError LispVal)) :=
     ("/", numericBinop (fun x y => Nat.div x y)),
     ("mod", numericBinop (fun x y => Nat.mod x y)),
     -- TODO: remainder?
+    ("=", numBoolBinop (· == ·)),
+    ("<", numBoolBinop (· < ·)),
+    (">", numBoolBinop (· > ·)),
+    ("/=", numBoolBinop (· != ·)),
+    (">=", numBoolBinop (· >= ·)),
+    ("<=", numBoolBinop (· <= ·)),
+    ("&&", boolBoolBinop (· && ·)),
+    ("||", boolBoolBinop (· || ·)),
+    ("string=?", strBoolBinop (· == ·)),
+    ("string<?", strBoolBinop (· < ·)),
+    ("string>?", strBoolBinop (· > ·)),
+    ("string<=?", strBoolBinop (· <= ·)),
+    ("string>=?", strBoolBinop (· >= ·)),
   ]
 
 def apply (func : String) (args : List LispVal) : ThrowsError LispVal :=
@@ -77,6 +112,11 @@ def rep : String → String := fun input => extractValue ∘ trapError $ do
 #guard rep "(quote (- 15))" == "(- 15)"
 #guard rep "(string->symbol \"flying-fish\")" == "flying-fish"
 #guard rep "(symbol->string martin)" == "\"martin\""
+#guard rep "(< 2 3)" == "#t"
+#guard rep "(> 2 3)" == "#f"
+#guard rep "(>= 3 3)" == "#t"
+#guard rep "(string=? \"test\" \"test\")" == "#t" -- TODO: extra ws breaks this one
+#guard rep "(string<? \"abc\" \"bba\")" == "#t"
 
 -- error handling
 #guard rep "(+ 2 \"two\")" == "Invalid type: expected number, found \"two\""
